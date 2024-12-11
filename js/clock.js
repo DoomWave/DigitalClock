28a79dd1dbee3607f938176f9d02f61f0889e063
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-12-10 22:45:51 -0800

    advancing with the alarm

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,9 @@
 class Clock {
-  constructor(timeDiv) {
+  constructor(timeDiv, alarmDiv, alarmTime, alarmAudio) {
     this.timeDiv = timeDiv;
+    this.alarmDiv = alarmDiv;
+    this.alarmTime = alarmTime;
+    this.alarmAudio = document.querySelector(this.alarmDiv + ' #alarmAudio')
     let tim = document.querySelector(this.timeDiv);
     let t = new Date();
     let time = t.toLocaleTimeString([], {
@@ -9,6 +12,8 @@ class Clock {
       second: "2-digit",
     });
     tim.innerHTML = time;
+
+    this.setAlarm()
 
     setInterval(this.updateTime.bind(this), 1000);
   }
@@ -22,7 +27,24 @@ class Clock {
       second: "2-digit",
     });
     tim.innerHTML = time;
+
+    if(time == this.alarmTime){
+      this.playAlarm()
+    }
+  }
+
+  setAlarm() {
+    const alarm = document.querySelector(this.alarmDiv + ' span ')
+    alarm.innerText = `Alarm (${this.alarmTime})`
+    
+  }
+
+  playAlarm() {
+    this.alarmAudio.currentTime = 0
+    this.alarmAudio.muted = false
+    this.alarmAudio.volume = .5
+    this.alarmAudio.play()
   }
 }
 
-new Clock("#time");
+new Clock('#time', 'alarm', '10:50:00 PM');
