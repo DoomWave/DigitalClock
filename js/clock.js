e32c9450bf880e8638eecdbe5347c84c2f31cb68
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-12-10 17:04:31 -0800

    using prettier to organize the text

--- js/clock.js
+++ js/clock.js
@@ -1,20 +1,28 @@
 class Clock {
-    constructor(timeDiv) {
-        this.timeDiv = timeDiv
-        let tim = document.querySelector(this.timeDiv)
-        let t = new Date()
-        let time = t.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'})
-        tim.innerHTML = time
+  constructor(timeDiv) {
+    this.timeDiv = timeDiv;
+    let tim = document.querySelector(this.timeDiv);
+    let t = new Date();
+    let time = t.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    tim.innerHTML = time;
 
-        setInterval(this.updateTime.bind(this), 1000)
-    }
+    setInterval(this.updateTime.bind(this), 1000);
+  }
 
-    updateTime(){
-        let tim = document.querySelector(this.timeDiv)
-        let t = new Date()
-        let time = t.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'})
-        tim.innerHTML = time
-    }
+  updateTime() {
+    let tim = document.querySelector(this.timeDiv);
+    let t = new Date();
+    let time = t.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    tim.innerHTML = time;
+  }
 }
 
-new Clock('#time')
+new Clock("#time");
